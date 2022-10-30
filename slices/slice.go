8a3cf219f45d87e0cf8f6df795b4f57adda2ac36
vjabuilds <jovan.vjestica@outlook.com>
2022-10-30 16:46:43 +0100

    Added slices example

---
+++ slices/slice.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	greeting := "Hello world!"
+	fmt.Println("The whole greeting is", greeting)
+	fmt.Println("The first part of the greeting is", greeting[:5])
+	fmt.Println("The second part of the greeting is", greeting[5:])
+	fmt.Println("The middle part of the greeting is", greeting[2:7])
+}
